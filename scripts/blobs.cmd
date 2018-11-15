b8ecaf87a4a92286c45f6f5ed2e4770a2dcf976e
vlad valko <vlad.valko@gmail.com>
2018-11-15 12:14:36 +0200

    fin blob3 key file out of directory

--- scripts/blobs.cmd
+++ scripts/blobs.cmd
@@ -11,6 +11,7 @@ call set location=eastus2
 call set storageaccname=blobbtorageaccount%version%
 call set SKU=Standard_LRS
 call set containername=blobcontainername%version%
+call set keyfile=..\..\key.txt
 
 
 call az group create  --name %ResourceGroupName%  --location %location%
@@ -19,8 +20,9 @@ call az storage account create --name %storageaccname%  --resource-group %Resour
 call set storageacckey=az storage account keys list --account-name %storageaccname%   --query "[?keyName=='key1'].value" -o tsv
 call set AZURE_STORAGE_ACCESS_KEY=%storageacckey%
 call set AZURE_STORAGE_ACCOUNT=%storageaccname%
-call %AZURE_STORAGE_ACCESS_KEY% >key.txt
+call %AZURE_STORAGE_ACCESS_KEY% >%keyfile%
 call az storage container create --name %containername%
-call set /P keystring=<key.txt
-call echo %keystring%
+call set /P keystring=<%keyfile%
+
 call AzCopy /Source:C:\Users\buster\Documents\GitHub\azure /Dest:https://%storageaccname%.blob.core.windows.net/%containername% /DestKey:%keystring% /S
+call az storage blob list --container-name %containername% -o table
